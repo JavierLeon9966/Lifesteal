245e6f0ff00db09946bf5f7c1d553b279ee26465
Covered123 <58715544+JavierLeon9966@users.noreply.github.com>
2022-02-05 15:55:56 -0300

    Finished plugin

---
+++ src/Main.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace JavierLeon9966\Lifesteal;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocletmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+final class Main extends PluginBase implements Listener{
+
+	public function onEnable(): void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	/**
+	 * @priority MONITOR
+	 */
+	public function onPlayerDeath(PlayerDeathEvent $event): void{
+		$player = $event->getPlayer();
+		$player->setMaxHealth($player->getMaxHealth() - 1);
+		
+		$lastDamageCause = $player->getLastDamageCause();
+		if($lastDamageCause instanceof EntityDamageByEntityEvent){
+			$entity = $lastDamageCause->getEntity();
+			if($entity instanceof Player){
+				$entity->setMaxHealth($entity->getMaxHealth() + 1);
+			}
+		}
+		if($player->getMaxHealth() === 0){
+			if($player->kick('You lost all your healths')){
+				$player->getServer()->getNameBans()->addBan($player->getName(), 'Lost all healths');
+			}
+		}
+	}
+}
